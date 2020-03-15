bfe4e73b8a8fe56726c6814de26e88f2bccc0e86
BaekDorazi <qorehgud@gmail.com>
2020-03-15 10:47:07 +0900

    기능추가, UI수정 / 액티비티에 플래시 on/off Switch 추가, 카메라 플래시 기능 추가

--- app/src/main/java/com/baek/flashlight/MainActivity.kt
+++ app/src/main/java/com/baek/flashlight/MainActivity.kt
@@ -2,11 +2,19 @@ package com.baek.flashlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val torch = Torch(this)
+
+        flash_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) torch.flashOn()
+            else torch.flashOff()
+        }
     }
 }
